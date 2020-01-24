4197cdcfd6014b48bc332fda56df28bbef8b7704
emilionito616 <emilionito9@gmail.com>
2020-01-24 16:45:13 -0600

    Curso angular seccion 1

---
+++ app.js
@@ -0,0 +1,12 @@
+function activar(quien, objeto, momento) {
+    if (objeto === void 0) { objeto = "batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activo la " + objeto + " en la " + momento;
+    }
+    else {
+        mensaje = quien + " activo la " + objeto;
+    }
+    console.log(mensaje);
+}
+activar("Emilio", "batiseñal", "tarde");
